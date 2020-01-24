0c834a8eafb58e0a1e73c177150717b8d952a262
SergeyKhrapin <khrapins@gmail.com>
2020-01-24 15:45:46 +0200

    Immediately Invoked Function Expression

---
+++ 001.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// let a = 5;
+
+// Immediately Invoked Function Expression
+(function () {
+    console.log('Result'); // text will be displayed in the console
+}())
+
+// But we got a TypeError - "use strict" is not a function
+// If we uncomment line 3 - there will be no error
